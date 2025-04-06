5cbad7f80b563d7e091f5788b12c1442863a41ed
mgr-inz-rafal <rchabowski@gmail.com>
2025-04-06 11:29:38 +0200

    Stub for future amount cache pruning

--- src/db/in_mem.rs
+++ src/db/in_mem.rs
@@ -6,15 +6,24 @@ pub(crate) enum Error {
     AlreadyExists,
 }
 
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub(super) enum PruningStrategy {
+    Ttl { duration: std::time::Duration },
+    Size { max_size: usize },
+}
+
 #[derive(Debug, Clone)]
 pub(crate) struct AmountCache<MonetaryValue> {
     txs: HashMap<u32, MonetaryValue>,
+    pruning_strategy: Option<PruningStrategy>,
 }
 
 impl<MonetaryValue> AmountCache<MonetaryValue> {
     pub(crate) fn new() -> Self {
         Self {
             txs: HashMap::new(),
+            pruning_strategy: None,
         }
     }
 }
@@ -27,12 +36,22 @@ impl<MonetaryValue> ValueCache<MonetaryValue> for AmountCache<MonetaryValue> {
     }
 
     fn insert(&mut self, id: u32, amount: MonetaryValue) -> Result<(), Self::Error> {
+        if let Some(strategy) = &self.pruning_strategy {
+            match strategy {
+                PruningStrategy::Size { max_size: _ } | PruningStrategy::Ttl { duration: _ } => {
+                    // TODO: Implement proper pruning strategy
+                }
+            }
+        }
+
         match self.txs.insert(id, amount) {
             Some(_) => Err(Error::AlreadyExists),
             None => Ok(()),
         }
     }
 
+    #[allow(dead_code)]
+    // To could be helpful when pruning is implemented.
     fn remove(&mut self, id: u32) -> Option<MonetaryValue> {
         self.txs.remove(&id)
     }
--- src/db/traits.rs
+++ src/db/traits.rs
@@ -4,5 +4,8 @@ pub trait ValueCache<MonetaryValue> {
 
     fn get(&self, id: &u32) -> Option<&MonetaryValue>;
     fn insert(&mut self, id: u32, amount: MonetaryValue) -> Result<(), Self::Error>;
+
+    #[allow(dead_code)]
+    // To could be helpful when pruning is implemented.
     fn remove(&mut self, id: u32) -> Option<MonetaryValue>;
 }
